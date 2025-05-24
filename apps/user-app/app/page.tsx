4ea2d6006b05d2907296ea1907922ba90f414501
maruf-rahman007 <rahman15-6212@s.diu.edu.bd>
2025-05-24 21:14:40 +0600

    feat : added auth and recoil

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -3,9 +3,10 @@ import Image, { type ImageProps } from "next/image";
 import styles from "./page.module.css";
 import { Button } from "@repo/ui/button";
 import Appbar from "@repo/ui/appbar";
+;
 
 
-export default function Home() {
+export default function ClientHome() {
   return (
     <>
       <Appbar/>
---
+++ apps/user-app/components/balance.tsx
@@ -0,0 +1,10 @@
+// "use client";
+
+// import { useBalance } from "@repo/store/balance";
+
+// export default function Balance() {
+//   const balance = useBalance();
+//   return <div>
+//     hi there {balance}
+//   </div>
+// }

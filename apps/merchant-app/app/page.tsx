a688ebbdacc5a469caf1fceed34686aa99e857ec
maruf-rahman007 <rahman15-6212@s.diu.edu.bd>
2025-05-25 23:19:05 +0600

    feat : added merchant app

---
+++ apps/merchant-app/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="flex min-h-screen flex-col items-center justify-center">
+        <h1 className="text-3xl font-bold underline">
+          Welcome to the Merchant App!
+        </h1>
+      </div>
+    </>
+  );
+}
--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -1,15 +1,15 @@
-import client from "@repo/db/client"
-import { NextResponse } from "next/server"
+// import client from "@repo/db/client"
+// import { NextResponse } from "next/server"
 
-export const GET = async () => {
-    const data = await client.user.create({
-        data: {
-            email: "test@example.com",
-            name: "maruf"
-        }
-    });
-    return NextResponse.json({
-        message: "hi there",
-        data
-    });
-};
+// export const GET = async () => {
+//     const data = await client.user.create({
+//         data: {
+//             email: "test@example.com",
+//             name: "maruf"
+//         }
+//     });
+//     return NextResponse.json({
+//         message: "hi there",
+//         data
+//     });
+// };

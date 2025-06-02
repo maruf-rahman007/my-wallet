813b7975d82a548465d3b134e4aa0683e211e070
maruf-rahman007 <rahman15-6212@s.diu.edu.bd>
2025-06-02 21:16:34 +0600

    Part 1 finish

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,11 +1,26 @@
 import ClientAppbar from "../components/appbarclient";
+import { getServerSession } from "next-auth";
+import { redirect } from 'next/navigation'
+import { authOptions } from "./lib/auth";
 
 
-export default function Home() {
 
-  return (
-    <>
-      <ClientAppbar />
-    </>
-  );
+
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  if (session?.user) {
+    redirect('/dashboard')
+  } else {
+    redirect('/api/auth/signin')
+  }
+  
 }
+
+// export default function Home() {
+
+//   return (
+//     <>
+//       <ClientAppbar />
+//     </>
+//   );
+// }
